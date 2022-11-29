dc7d23054c8d17ac68bcbf25b2413a6a23475b09
Smoke5643 <smoke5643@gmail.com>
2022-11-28 21:01:31 -0500

    worked on syntax and typos, added to console.log and a prompt function for how long the password needs to be

--- script.js
+++ script.js
@@ -2,11 +2,46 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-    TODO:
-    var specialCharacter = "!#$%&()*+,-./:;<=>?@[]^_`{|}~"
-    var letters = "abcdefghijklmnopqrstuvwxyz"
-    var numbers = "0123456789"
-    var upperLetters = letters.toUpperCase()
+    var specialCharacters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+    var letters = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "0123456789";
+    var upperLetters = letters.toUpperCase();
+    var isUpperCase = confirm('Would you like to use uppercase?');
+    var isLowerCase = confirm('Would you like to use lowercase?');
+    var isSpecial = confirm('Would you like to use special characters?');
+    var isNumber = confirm('Would you like to use numbers?');
+    var howLong = prompt('Between 8-128 characters, how long should the password be?');
+    var allCharacters = "";
+
+    if (isUpperCase){
+        console.log('uppercase');
+        allCharacters += upperLetters;
+    }
+  
+    if (isLowerCase){
+        console.log('lowercase');
+        allCharacters += letters;
+    }
+  
+    if (isSpecial){
+        console.log('special');
+        allCharacters += specialCharacters;
+    }
+  
+    if (isNumber){
+        console.log('numbers');
+        allCharacters += numbers;
+    }
+
+    if (howLong){
+        console.log('how long');
+        console.log(howLong);
+    }
+  
+    //grab random letters
+    console.log(allCharacters);
+
+    return "";
 
 }
 
