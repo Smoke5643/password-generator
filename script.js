13dbcecc451b958ae33ae04b7167da5038e02b16
Smoke5643 <smoke5643@gmail.com>
2022-11-29 21:59:39 -0500

    added howLong and logic to test password generation

--- script.js
+++ script.js
@@ -10,38 +10,41 @@ function generatePassword(){
     var isLowerCase = confirm('Would you like to use lowercase?');
     var isSpecial = confirm('Would you like to use special characters?');
     var isNumber = confirm('Would you like to use numbers?');
-    var howLong = prompt('Between 8-128 characters, how long should the password be?');
-    var allCharacters = "";
+    var howLong = Number(prompt('Between 8-128 characters, how long should the password be?'));
+    var userPassword = '';  
+    var random = Math.floor(Math.random() * userPassword.length);
+    
+
 
     if (isUpperCase){
         console.log('uppercase');
-        allCharacters += upperLetters;
+        userPassword += upperLetters;
     }
   
     if (isLowerCase){
         console.log('lowercase');
-        allCharacters += letters;
+        userPassword += letters;
     }
   
     if (isSpecial){
         console.log('special');
-        allCharacters += specialCharacters;
+        userPassword += specialCharacters;
     }
   
     if (isNumber){
         console.log('numbers');
-        allCharacters += numbers;
+        userPassword += numbers;
     }
 
-    if (howLong){
-        console.log('how long');
+    if (howLong >= 8 && howLong <= 128){
         console.log(howLong);
-    }
-  
-    //grab random letters
-    console.log(allCharacters);
+        userPassword = random(howLong);
+    }   else{
+        alert('Password must be between 8 and 128 characters long.');
+        return '';
+        }
 
-    return "";
+    return userPassword;
 
 }
 
