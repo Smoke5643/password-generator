a006e771be3f053068c4c8807cc72cc9f0d8a37e
Smoke5643 <smoke5643@gmail.com>
2022-11-30 21:52:29 -0500

    added for loop to return random password

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-    var specialCharacters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
-    var letters = "abcdefghijklmnopqrstuvwxyz";
-    var numbers = "0123456789";
+    var specialCharacters = '!#$%&()*+,-./:;<=>?@[]^_`{|}~';
+    var letters = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
     var upperLetters = letters.toUpperCase();
     var isUpperCase = confirm('Would you like to use uppercase?');
     var isLowerCase = confirm('Would you like to use lowercase?');
     var isSpecial = confirm('Would you like to use special characters?');
     var isNumber = confirm('Would you like to use numbers?');
-    var howLong = Number(prompt('Between 8-128 characters, how long should the password be?'));
-    var userPassword = '';  
+    var howLong = prompt('Between 8-128 characters, how long should the password be?');
+    var possiblePassword = '';  
     var random = '';
+    var userPassword = '';
     
-
-
+    if (howLong < 8 || howLong > 128){
+        alert ('Password must be between 8 and 128 characters');
+        return "";
+    }
+    
     if (isUpperCase){
-        console.log('uppercase');
-        userPassword += upperLetters;
+        possiblePassword += upperLetters;
     }
   
     if (isLowerCase){
-        console.log('lowercase');
-        userPassword += letters;
+        possiblePassword += letters;
     }
   
     if (isSpecial){
-        console.log('special');
-        userPassword += specialCharacters;
+        possiblePassword += specialCharacters;
     }
   
     if (isNumber){
-        console.log('numbers');
-        userPassword += numbers;
+        possiblePassword += numbers;
+    }
+    
+    if (possiblePassword = ' '){
+        alert('You must choose at least one character type')
+        br
     }
 
-    if (howLong >= 8 && howLong <= 128){
-        random = Math.floor(Math.random() * userPassword.length);
-        userPassword = userPassword[random];
-        }   else{
-        alert('Password must be between 8 and 128 characters long.');
-        return '';
+    for (var i = 0; i < howLong; i++){
+        random = Math.floor(Math.random() * possiblePassword.length);
+        userPassword += possiblePassword[random];
         }
-
+    
     return userPassword;
 
 }
